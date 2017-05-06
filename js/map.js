5a3dc5f049044710b411eda6354692c5c97c38f4
valereekalinina <valereekalinina@gmail.com>
2017-05-06 21:16:17 +0300

    add js

--- js/map.js
+++ js/map.js
@@ -1,1 +1,24 @@
-<script src="http://api-maps.yandex.ru/2.0/?load=package.full&amp;lang=ru-RU" type="text/javascript"></script>
+ymaps.ready(init);
+var myMap;
+var myPlacemark;
+
+function init() {
+  myMap = new ymaps.Map("map" , {
+    center: [59.93632956, 30.32173485],
+    zoom: 16
+  });
+
+  myPlacemark = new ymaps.Placemark([59.93632956, 30.32173485], {
+    balloonContentHeader: '<img src="img/pin.png">',
+    balloonContentBody: "Gllacy Shop",
+    hintContent: "Gllacy Shop",
+  },
+  {
+      iconLayout: "default#image",
+      iconImageHref: "./img/icons/pin.png",
+      iconImageSize: [80, 140],
+      iconImageOffset: [-40, -95]
+  });
+
+  myMap.geoObjects.add(myPlacemark);
+}
